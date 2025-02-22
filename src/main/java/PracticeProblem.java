0e23728331c645a82a1df5821b45851e3c23119f
kelly-fu-ics4u1-02-25 <kelly.fu@student.tdsb.on.ca>
2025-02-22 03:54:11 +0000

    Update but not finished

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,30 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+	}
+	
+	public static String pyramid(int rows) {
+		String result = "";
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < rows - 1 - i; j++) {
+				result += " ";
+			}
+			//result += "*";
+			//for asodfjsokadfj result += " *";
+			//" * *\n* * *"
+			//if (i != rows - 1) result += '\n';
+			for (int j = 0; j <= i; j++) {
+				result += "* ";
+			}
+			if (i != rows - 1) result += "\n";
+			
+		}
+		return result;
+	}
+	 
 
+	public static boolean hasLowercase (String letter){
+		for ()
 	}
 
 }

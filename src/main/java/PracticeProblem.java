764dfd1d751ec89f35f8896c28ce00af465e6bd5
kelly-fu-ics4u1-02-25 <kelly.fu@student.tdsb.on.ca>
2025-02-24 15:14:48 +0000

    update but not correct

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,28 +3,47 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 	}
 	
-	public static String pyramid(int rows) {
-		String result = "";
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < rows - 1 - i; j++) {
-				result += " ";
-			}
-			//result += "*";
-			//for asodfjsokadfj result += " *";
-			//" * *\n* * *"
-			//if (i != rows - 1) result += '\n';
-			for (int j = 0; j <= i; j++) {
-				result += "* ";
-			}
-			if (i != rows - 1) result += "\n";
-			
-		}
-		return result;
+	//1
+	public static String pyramid (int rows){
+	String o ="";
+	for (int i = 0; i < rows; i++){
+	for (int j = rows - 1; j > 1; j--) {
+		//System.out.print(" ");
+		o = o+" ";
 	}
-	 
+	for (int j = 0; j <= i; j++){
+		//System.out.print("* ");
+		o = o+"* ";
+	}
+	//System.out.println();
+	o = o+"\n";
+	}
+     return o;
+}
 
-	public static boolean hasLowercase (String letter){
-		for ()
+	 
+	//2
+	public static void square(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == 0 || i == size - 1 || j == 0 || j == size - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+	}
+
+	//3
+	public static boolean hasLowercase (String word){
+		for (int i = 0; i <= word.length(); i++){
+			if (Character.isLowerCase(word.charAt(i))){
+				return true;
+			}
+		}
+		return false;
 	}
 
 }

9af650fcf26030314d7c28549c282b289a91dda5
kelly-fu-ics4u1-02-25 <kelly.fu@student.tdsb.on.ca>
2025-03-05 04:59:38 +0000

    Update correct answers (finally)

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -5,45 +5,43 @@ public class PracticeProblem {
 	
 	//1
 	public static String pyramid (int rows){
-	String o ="";
-	for (int i = 0; i < rows; i++){
-	for (int j = rows - 1; j > 1; j--) {
-		//System.out.print(" ");
-		o = o+" ";
+		String drawing = "";
+		for (int i = rows - 1; i >= 0; --i){
+			drawing += " ".repeat(i) + '*';
+			drawing += " *".repeat(rows - 1 -i);
+			if (i != 0){
+				drawing += "\n";
+			}
+		}
+		return drawing;
 	}
-	for (int j = 0; j <= i; j++){
-		//System.out.print("* ");
-		o = o+"* ";
-	}
-	//System.out.println();
-	o = o+"\n";
-	}
-     return o;
-}
 
-	 
 	//2
-	public static void square(int size) {
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (i == 0 || i == size - 1 || j == 0 || j == size - 1) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
+	public static String square(int length){
+		if (length < 0){
+			return "";
+		}
+
+		String drawing = "*".repeat(length);
+		if (length > 1){
+			for (int i = 0; i < length - 2; ++i){
+			drawing += "\n*" + " ".repeat(length - 2) + '*';
+			}
+			drawing += '\n' + "*".repeat(length);
+		}
+		return drawing;
 	}
+
 
 	//3
-	public static boolean hasLowercase (String word){
-		for (int i = 0; i <= word.length(); i++){
+	public static boolean hasLowercase(String word){
+		for (int i = 0; i < word.length(); i++){
 			if (Character.isLowerCase(word.charAt(i))){
 				return true;
 			}
 		}
 		return false;
-	}
+	} 
+
 
 }
